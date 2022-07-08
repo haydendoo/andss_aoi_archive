b93eec7e96396c463db64a55a9ef066ee99ae426
haydendoo <100998299+haydendoo@users.noreply.github.com>
2022-07-09 00:42:13 +0800

    Update sample_testcase_generator.cpp

--- sample_testcase_generator.cpp
+++ sample_testcase_generator.cpp
@@ -35,8 +35,6 @@ void create(string n){
 }
 
 int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
 	// Sample test case
 	ofstream input("input/input00.txt");
 	ofstream output("output/output00.txt");
